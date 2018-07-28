6facb5dfb263365dcb1a4562e09cc81f6970badf
mrko11 <m.mariko.nov10@gmail.com>
2018-07-28 13:31:34 +0900

    api test

--- public_html/api.php
+++ public_html/api.php
@@ -1,21 +1,18 @@
 <?php
 date_default_timezone_set('Asia/Singapore');
-# ↑ タイムゾーンをセット
- 
-$name = isset($_POST['name']) ? htmlspecialchars($_POST["name"], ENT_QUOTES) :  "no name posted";
-$uni = isset($_POST['uni']) ? htmlspecialchars($_POST["uni"], ENT_QUOTES) : "no uni name posted";
+# Set time date_default_timezone_set
+
+$name = isset($_POST['name']) ? htmlspecialchars($_POST["name"], ENT_QUOTES) :  "employeeA";
+$r_eve = "coffee break on 31-Jul";
 # ↑ POST送信を処理。三項演算子を用いて、中身が入っていないときは入っていないことを明示的にしている。isset関数は、中身が入っているか判断している
- 
+
 $json_array = array(
     'time' => date("Y-m-d H:i:s"),
     'name' => $name,
-    'university' => $uni,
+    'recommended_event' => $r_eve,
 );
-# ↑ JSON 形式にする
- 
+
 header("Content-Type: text/javascript; charset=utf-8");
-# ↑ 半分おまじない。JSONで送りますよという合図
- 
+
 echo json_encode($json_array);
-# ↑ JSON 形式にエンコードしてechoでPOST送信
 ?>
